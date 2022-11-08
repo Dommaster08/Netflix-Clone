236c989a1b3e8ddfcb6611e52baa86c0312e5190
Luan <luancx7@gmail.com>
2022-11-07 21:33:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+let ask1 = document.getElementById('ask1')
+let ask2 = document.getElementById('ask2')
+let ask3 = document.getElementById('ask3')
+let ask4 = document.getElementById('ask4')
+let ask5 = document.getElementById('ask5')
+let ask6 = document.getElementById('ask6')
+
+let toggle = false
+
+
+function mostrarText(){
+    if(toggle == false){
+        ask1.style.display = 'block'
+        ask1.style.backgroundColor = '#2e2e2e'
+        toggle = true
+    }else if(toggle == true){
+        ask1.style.display = 'none'
+        toggle = false
+    }
+   
+}
+
+function mostrarText2(){
+    
+    if(toggle == false){
+        ask2.style.display = 'block'
+        ask2.style.backgroundColor = '#2e2e2e'
+        toggle = true
+    }else if(toggle == true){
+        ask2.style.display = 'none'
+        toggle = false
+    }
+   }
+
+
+function mostrarText3(){
+    if(toggle == false){
+        ask3.style.display = 'block'
+        ask3.style.backgroundColor = '#2e2e2e'
+        toggle = true
+    }else if(toggle == true){
+        ask3.style.display = 'none'
+        toggle = false
+    }
+   }
+
+function mostrarText4(){
+    if(toggle == false){
+        ask4.style.display = 'block'
+        ask4.style.backgroundColor = '#2e2e2e'
+        toggle = true
+    }else if(toggle == true){
+        ask4.style.display = 'none'
+        toggle = false
+    }
+   }
+
+function mostrarText5(){
+    if(toggle == false){
+        ask5.style.display = 'block'
+        ask5.style.backgroundColor = '#2e2e2e'
+        toggle = true
+    }else if(toggle == true){
+        ask5.style.display = 'none'
+        toggle = false
+    }
+   }
+
+function mostrarText6(){
+    if(toggle == false){
+        ask6.style.display = 'block'
+        ask6.style.backgroundColor = '#2e2e2e'
+        toggle = true
+    }else if(toggle == true){
+        ask6.style.display = 'none'
+        toggle = false
+    }
+   }
+
+
+
